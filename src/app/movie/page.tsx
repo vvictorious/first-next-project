2be5d005c5e1ace9b61956a39d5d36852e7f50f8
vvictorious <vvictorious30@gmail.com>
2024-02-25 10:23:44 -0800

    movie component is being mapped through and rendered with the correct data from api

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
 
-export default function Movie() {
-    return (
-      <main>
-        Recipe Page
-      </main>
-    );
-  }  
+interface MovieProps {
+  title: string;
+  id: string;
+  poster_path: string;
+  release_date: string;
+}
+
+export default function Movie({
+  title,
+  id,
+  poster_path,
+  release_date,
+}: MovieProps) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>Release Date</h2>
+      <Link href={"/asd"}>
+        <img src="" />
+      </Link>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import Movie from "./movie/page";
 
 export default async function Home() {
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
-  const res = await data.json()
-  console.log(res)
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  console.log(res);
   return (
     <main>
-      Home Page
+      {res.results.map((movie: any) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          release_date={movie.release_date}
+        />
+      ))}
     </main>
   );
 }
